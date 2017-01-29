9ace7def87972bf332f4fe78c3dec1ee5a555d86
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2017-01-30 01:25:19 +0200

    problems

--- app/Problem.php
+++ app/Problem.php
@@ -4,9 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Input;
 
 /**
  * Class Problem
@@ -54,8 +53,8 @@ class Problem extends Model
             'name' => 'required|string|max:255',
             'description' => 'required|string|max:3000',
             'difficulty' => 'required|integer|between:0,5',
-            'archive' => 'mimetypes:application/x-gzip',
-            'volumes' => 'array'
+            'archive' => 'required,mimetypes:application/x-gzip',
+            //'volumes' => 'array'
         ];
     }
 
@@ -76,21 +75,21 @@ class Problem extends Model
 
         return $dir;
     }
-
+/*
     public static function getAlternateFilePath()
     {
         return base_path('var/test_db/');
-    }
+    }*/
 
-    public function volumes()
+/*    public function volumes()
     {
         return $this->belongsToMany('App\Volume');
-    }
+    }*/
 
-    public function contests()
+/*    public function contests()
     {
         return $this->belongsToMany(Contest::class, 'contest_problem', 'problem_id', 'contest_id')->withTimestamps();
-    }
+    }*/
 
     public function setImage($name)
     {
@@ -129,111 +128,6 @@ class Problem extends Model
 
     public function getArchivePath() {
         return $this->getFilePath() . $this->attributes['archive'];
-    }
-
-    public function getLink(Contest $contest) {
-        return route('frontend::contests::problem', ['contest_id' => $contest->id, 'problem_id' => $this->id]);
-    }
-
-    public function getSolutions(Contest $contest) {
-        return $contest->solutions()->where('problem_id', $this->id)->get();
-    }
-
-    public function getAVGScore(Contest $contest) {
-        return $contest
-            ->solutions()
-            ->select(\DB::raw('SUM(success_percentage / 100 * contest_problem.max_points) as total'))
-            ->join('contest_problem', 'solutions.problem_id', '=', 'contest_problem.problem_id')
-            ->where('solutions.problem_id', $this->id)
-            ->first()->total;
-    }
-
-    public function getUsersWhoTryToSolve(Contest $contest) {
-        return $contest
-            ->solutions()
-            ->select(DB::raw('count(*) as item'))
-            ->where('problem_id', $this->id)
-            ->groupBy('user_id', 'problem_id')
-            ->get()
-            ->count();
-    }
-
-    public function getUsersWhoSolved(Contest $contest) {
-        if($contest->show_max) {
-            return $contest
-                ->solutions()
-                ->select(DB::raw('count(*) as item'))
-                ->where('problem_id', $this->id)
-                ->where('status', Solution::STATUS_OK)
-                ->groupBy('user_id', 'problem_id')
-                ->get()
-                ->count();
-        }
-
-        return $contest
-            ->solutions()
-            ->select(DB::raw('count(*) as item'))
-            ->where('problem_id', $this->id)
-            ->groupBy('user_id', 'created_at')
-            ->take(1)
-            ->get()
-            ->count();
-    }
-
-
-    private function getContestSolutionQuery($contest_id)
-    {
-        return DB::table('solutions')
-            ->join('contest_solution', 'solution_id', '=', 'id')
-            ->where('problem_id', '=', $this->attributes['id'])
-            ->where('contest_id', '=', $contest_id);
-    }
-
-    private function getMaxPointsSolution($query)
-    {
-        return $query->orderBy('success_percentage', 'desc')
-            ->first();
-    }
-
-    private function getLatestSolution($query)
-    {
-        return $query->orderBy('created_at', 'desc')
-            ->first();
-    }
-
-    public function getContestDisplaySolution(Contest $contest)
-    {
-        $solution = $this->getContestSolutionQuery($contest->id);
-        if ($contest->show_max) {
-            $solution = $this->getMaxPointsSolution($solution);
-        } else {
-            $solution = $this->getLatestSolution($solution);
-        }
-        return $solution;
-    }
-
-    public function getContestUserDisplaySolution(Contest $contest, $user_id)
-    {
-        $solution = $this->getContestSolutionQuery($contest->id)->where('user_id', $user_id);
-        if ($contest->show_max) {
-            $solution = $this->getMaxPointsSolution($solution);
-        } else {
-            $solution = $this->getLatestSolution($solution);
-        }
-
-        return $solution;
-    }
-
-    public function getPointsString(Contest $contest)
-    {
-        $display_solution = $this->getContestDisplaySolution($contest);
-        if ($display_solution) {
-            $points_string = $display_solution->success_percentage / 100 * $contest->getProblemMaxPoints($this->id);
-        } else {
-            $points_string = '-';
-        }
-        $points_string .= ' / ' . $contest->getProblemMaxPoints($this->id);
-        return $points_string;
     }
 
     public static function search($term, $page)
--- resources/views/problems/form.blade.php
+++ resources/views/problems/form.blade.php
@@ -81,7 +81,7 @@
                                 @endif
                             </div>
                         </div>
-
+{{--
                         <div class="form-group row{{ $errors->has('volumes') ? ' has-danger' : '' }}">
                             <label class="form-control-label col-md-3 col-sm-3 col-xs-12" for="volumes">Volumes</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
@@ -97,7 +97,7 @@
                                 @endif
                             </div>
                         </div>
-
+--}}
                         <div class="ln_solid"></div>
                         <div class="form-group row">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
